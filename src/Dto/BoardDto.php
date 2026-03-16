3e8347b9d7e596b3cca66ccf0bb8f02ea128a31e
Tobias Brogle <135111039+Astro2006@users.noreply.github.com>
2026-03-16 16:01:40 +0100

    wip

--- src/Dto/BoardDto.php
+++ src/Dto/BoardDto.php
@@ -7,30 +7,30 @@ use Illuminate\Support\Arr;
 class BoardDto
 {
     public function __construct(
-        public readonly string $id,
-        public readonly string $name,
-        public readonly ?string $description,
-        public readonly string $type,
-        public readonly string $viewLink,
-        public readonly ?string $teamId,
-        public readonly ?string $projectId,
-        public readonly ?string $createdAt,
-        public readonly ?string $modifiedAt,
+        public string $id,
+        public string $name,
+        public ?string $description,
+        public string $type,
+        public string $viewLink,
+        public ?string $teamId,
+        public ?string $projectId,
+        public ?string $createdAt,
+        public ?string $modifiedAt,
     ) {}
 
     /** @param array<string, mixed> $data */
     public static function fromResponse(array $data): self
     {
         return new self(
-            id: is_string($v = Arr::get($data, 'id', '')) ? $v : '',
-            name: is_string($v = Arr::get($data, 'name', '')) ? $v : '',
-            description: is_string($v = Arr::get($data, 'description')) ? $v : null,
-            type: is_string($v = Arr::get($data, 'type', 'board')) ? $v : '',
-            viewLink: is_string($v = Arr::get($data, 'viewLink', '')) ? $v : '',
-            teamId: is_string($v = Arr::get($data, 'team.id')) ? $v : null,
-            projectId: is_string($v = Arr::get($data, 'project.id')) ? $v : null,
-            createdAt: is_string($v = Arr::get($data, 'createdAt')) ? $v : null,
-            modifiedAt: is_string($v = Arr::get($data, 'modifiedAt')) ? $v : null,
+            id: Arr::get($data, 'id', ''),
+            name: Arr::get($data, 'name', ''),
+            description: Arr::get($data, 'description'),
+            type: Arr::get($data, 'type', 'board'),
+            viewLink: Arr::get($data, 'viewLink', ''),
+            teamId: Arr::get($data, 'team.id'),
+            projectId: Arr::get($data, 'project.id'),
+            createdAt: Arr::get($data, 'createdAt'),
+            modifiedAt: Arr::get($data, 'modifiedAt'),
         );
     }
 }
