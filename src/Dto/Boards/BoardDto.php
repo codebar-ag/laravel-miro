c506885e2e96df295361f058d6721bc7de678b90
Julian Leipert <juli.leipert@gmail.com>
2026-03-17 16:51:56 +0100

    wip

--- src/Dto/Boards/BoardDto.php
+++ src/Dto/Boards/BoardDto.php
@@ -2,7 +2,6 @@
 
 namespace CodebarAg\Miro\Dto\Boards;
 
-use Illuminate\Support\Arr;
 use WendellAdriel\ValidatedDTO\ValidatedDTO;
 
 class BoardDto extends ValidatedDTO
@@ -63,29 +62,21 @@ class BoardDto extends ValidatedDTO
         ];
     }
 
-    /** @param array<string, mixed> $data */
+    /**
+     * @param  array{id: string, name: string, description?: string|null, type?: string, viewLink?: string, team?: array{id?: string|null}, project?: array{id?: string|null}, createdAt?: string|null, modifiedAt?: string|null}  $data
+     */
     public static function fromResponse(array $data): self
     {
-        $id = Arr::get($data, 'id', '');
-        $name = Arr::get($data, 'name', '');
-        $description = Arr::get($data, 'description');
-        $type = Arr::get($data, 'type', 'board');
-        $viewLink = Arr::get($data, 'viewLink', '');
-        $teamId = Arr::get($data, 'team.id');
-        $projectId = Arr::get($data, 'project.id');
-        $createdAt = Arr::get($data, 'createdAt');
-        $modifiedAt = Arr::get($data, 'modifiedAt');
-
         return new self(
-            id: $id,
-            name: $name,
-            description: $description,
-            type: $type,
-            viewLink: $viewLink,
-            teamId: $teamId,
-            projectId: $projectId,
-            createdAt: $createdAt,
-            modifiedAt: $modifiedAt,
+            id: $data['id'],
+            name: $data['name'],
+            description: $data['description'] ?? null,
+            type: $data['type'] ?? 'board',
+            viewLink: $data['viewLink'] ?? '',
+            teamId: $data['team']['id'] ?? null,
+            projectId: $data['project']['id'] ?? null,
+            createdAt: $data['createdAt'] ?? null,
+            modifiedAt: $data['modifiedAt'] ?? null,
         );
     }
 }
